87a45d57fb29aec9261d4be0ce823de9be0da421
a18marcastru <a18marcastru@inspedralbes.cat>
2022-03-29 12:23:01 +0200

    Cinema

---
+++ vue-cinema/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <button class="btn btn-primary"><RouterLink id="session" to="/">Home</RouterLink></button>
+        <button><RouterLink to="/about">About</RouterLink></button>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+nav {
+  text-align: center;
+}
+#session {
+  margin: 5px;
+}
+</style>
---
+++ vue-cinema/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script>
+import Session from "@/components/Session.vue";
+export default {
+  components: {
+    Session
+  }
+}
+</script>
+
+<template>
+  <main>
+    <Session/>
+  </main>
+</template>
